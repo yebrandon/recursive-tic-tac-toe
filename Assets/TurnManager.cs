4864b084d52d1cbb3314d7b21392e6422b648a33
EatriceEveryday <nathanielcode@gmail.com>
2020-12-27 13:21:51 -0500

    Forgot to save the scene

--- Assets/TurnManager.cs
+++ Assets/TurnManager.cs
@@ -7,6 +7,9 @@ public class TurnManager : MonoBehaviour
     public string turnPlayer = "X";
     public TicTacToe father;
 
+    public Box hovering;
+    public int[,] nextTurnpath;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +22,27 @@ public class TurnManager : MonoBehaviour
         
     }
 
-    public void changeTurn(){
+    public void changeTurn(Box clicked){
+
         if(turnPlayer == "X"){
             turnPlayer = "O";
         }
         else{
             turnPlayer = "X";
+        }
+
+        int[,] nextTurnPath = new int[clicked.getPath().GetLength(0) - 1, 2];
+
+    }
+
+    public void getNextTurnPath(Box clicked, int[,] nextTurnPath)
+    {
+        int pathLength = clicked.getPath().GetLength(0);
+
+        for (int i = 1; i < pathLength; i++)
+        {
+            nextTurnPath[i - 1, 0] = clicked.getPath()[i, 0];
+            nextTurnPath[i - 1, 1] = clicked.getPath()[i, 1];
         }
     }
 }
